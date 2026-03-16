784a1c280fa6cd21e21dd6a9194a4ac924436bfb
PourRevenir <yyk39693232@outlook.com>
2026-03-17 02:18:18 +0800

    a new directory is added

--- Source/csharp/obj/Debug/net10.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ Source/csharp/obj/Debug/net10.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e8814b03ce289c312456f9343a8c5e7e456de0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
